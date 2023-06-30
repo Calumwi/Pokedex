2055de2beac82671ed3b0b2527a1eba06e69034d
Calum Wilmot <calumdefences@gmail.com>
2023-06-30 18:08:28 +0100

    Form and handler updated to display entire API response in JSON format

--- script.js
+++ script.js
@@ -1,24 +1,33 @@
 const numberInput = document.getElementById('number_input');
 let pokéNumber = numberInput.value;
+const form = document.querySelector('form');
+const textArea = document.getElementById("textArea")
+
 numberInput.addEventListener('input', function(e) {
     pokéNumber = e.target.value;
   })
 
-
-const form = document.querySelector('form');
 
 form.addEventListener('submit', function(e) {
   e.preventDefault();
   console.log('Submitted');
 
 
-axios.get('https://pokeapi.co/api/v2/pokemon/' + pokéNumber)
-.then(function(response) {
-  console.log(response.data);
+  axios.get('https://pokeapi.co/api/v2/pokemon/' + pokéNumber)
+  .then(function(response) {
+    // const pokéResponse = response.data;
+    textArea.textContent = ''
+    textArea.textContent = JSON.stringify(response)
+
+  })
+  .catch(function(error) {
+    console.log(error);
+  })
+  return false;  
 })
-.catch(function(error) {
-  console.log(error);
-})
-})
+
+// const displayReturn = (response) => {
+//   return response.data
+// }
 
 numberInput.value = '';
